e9add00527b9665af73d56d28029c48db3303ce2
loafa <38090954+ItzPikaExploits@users.noreply.github.com>
2021-08-23 18:28:20 -0700

    Create Nightmare's UI.lua

---
+++ Outlaster/Nightmare's UI.lua
@@ -0,0 +1,47 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ItzPikaExploits/scripts/main/UIUtilEdit.lua"))()
+
+local TribeNumber = 0
+
+local window = lib:CreateWindow("Nightmare's UI")
+
+function GetExistingTeam()
+    if (workspace:FindFirstChild("1")) then
+        return 1;
+    elseif (workspace:FindFirstChild("2")) then
+        return 2;
+    end
+end
+
+local FIND_ITEMS = window:AddFolder("Item Searching");
+local searchItems = FIND_ITEMS:AddButton({ 
+	text = "Search for Advantage",
+	callback = function()
+            local item = (function()
+                local toreturn = nil;
+                for _, c in pairs(workspace:FindFirstChild(tostring(TribeNumber)):GetChildren()) do
+                    if (c.Name == "Bush" and #c:GetChildren() > 0) then
+                        toreturn = c.Part.Part;
+                    end
+                end
+                return toreturn
+            end)()
+            game.Players.LocalPlayer.Character.Humanoid:MoveTo(item.Position)
+	end,
+});
+local TEAM_LABEL = FIND_ITEMS:AddLabel({text="MY TRIBE ISLAND IS..."})
+local isVoting = FIND_ITEMS:AddButton({ 
+	text = "VOTING",
+	callback = function()
+		TribeNumber = (GetExistingTeam() == 1 and 2 or 1)
+		TEAM_LABEL.Text = "MY TRIBE ISLAND IS... ".. TribeNumber
+	end,
+});
+local isSpectatingMerged = FIND_ITEMS:AddButton({ 
+	text = "SPECTATING / MERGED",
+	callback = function()
+		TribeNumber = GetExistingTeam();
+		TEAM_LABEL.Text = "MY TRIBE ISLAND IS... ".. TribeNumber
+	end,
+});
+
+lib:Init()
