757a4d7dc530024997996d95b7d6faaa1f4349c1
loafa <38090954+ItzPikaExploits@users.noreply.github.com>
2022-08-04 02:22:20 -0700

    Create RIU_ItemESP.lua

---
+++ Misc/RIU_ItemESP.lua
@@ -0,0 +1,47 @@
+local RunService = game:GetService("RunService")
+
+local TrackedItems = {};
+
+local Camera = workspace.CurrentCamera;
+local SpawnedItems = workspace.Map.Items.SpawnedItems;
+
+local function TrackItem(tool)
+    if (TrackedItems[tool]) then return end
+    local Label = Drawing.new("Text")
+    Label.Text = tool.Name
+    Label.Size = 20
+    Label.Color = Color3.new(1, 1, 1)
+    Label.Center = true
+    Label.Outline = true
+    Label.OutlineColor = Color3.new(0, 0, 0)
+    TrackedItems[tool] = { Label };
+    
+    tool.AncestryChanged:Connect(function()
+        if (not tool:IsDescendantOf(SpawnedItems)) then
+            local data = TrackedItems[tool];
+            local Label = data[1]
+            Label:Remove();
+            TrackedItems[tool] = nil;
+        end
+    end)
+end
+
+for _, c in pairs(SpawnedItems:GetChildren()) do
+    coroutine.wrap(TrackItem)(c);
+end
+SpawnedItems.ChildAdded:Connect(TrackItem);
+
+RunService.RenderStepped:Connect(function()
+    for t, info in pairs(TrackedItems) do
+        local handle = t:FindFirstChildWhichIsA("BasePart") or t:FindFirstChildWhichIsA("Attachment") or t:FindFirstChildWhichIsA("BillboardGui");
+        if (not handle) then continue; end
+        
+        local Label = info[1];
+        local Position = nil;
+        
+        local vector, onScreen = Camera:WorldToScreenPoint(Position)
+        local screenPoint = Vector2.new(vector.X, vector.Y)
+        Label.Position = screenPoint;
+        Label.Visible = onScreen;
+    end
+end)
