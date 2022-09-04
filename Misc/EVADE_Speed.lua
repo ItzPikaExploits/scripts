378220fea26dc335289f7fb50144c51e6541034b
loafa <38090954+ItzPikaExploits@users.noreply.github.com>
2022-09-04 06:52:48 -0700

    Update EVADE_Speed.lua

--- Misc/EVADE_Speed.lua
+++ Misc/EVADE_Speed.lua
@@ -3,7 +3,7 @@ if (_G.oldspeed) then _G.oldspeed:Disconnect() end
 _G.oldspeed = game:GetService("RunService").Stepped:Connect(function()
     local root = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
     if (root ~= nil) then
-         local newvel = root.Velocity/40;
+         local newvel = root.Velocity/50;
          root.CFrame += Vector3.new(newvel.X, 0, newvel.Z)
     end
 end)
